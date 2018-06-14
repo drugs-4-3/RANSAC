f92fa6892a4809748f1cdca8069339badcd9f4f0
michal.sciubidlo <michal.sciubidlo@limango.pl>
2018-06-14 22:55:12 +0200

    cleaned up a little bit

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import time
 def curr_time():
     return int(round(time.time() * 1000))
 
+
 def generate_neighbours():
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
@@ -26,6 +27,7 @@ def generate_neighbours():
     print("results count: " + str(len(asd)))
     print(asd)
 
+
 def draw_keypoints():
     img_file = sys.argv[1]
     keypoints_file = sys.argv[2]
@@ -34,20 +36,13 @@ def draw_keypoints():
     t1 = curr_time()
     asd = image_drawing.draw_keypoints(img_file, keypoints)
     t2 = curr_time()
+    print("execution took: " + str(t2 - t1) + " ms.")
+
 
 arguments = sys.argv
-
 if len(arguments) < 2:
     print("ERROR: not enough arguments for the program. Terminating.")
     exit()
-
-# img_filename = sys.argv[1]
-# features_filename = sys.argv[2]
-#
-# keypoints = feature_extractor.get_keypoints(features_filename)
-# image_drawing.draw_keypoints(img_filename, keypoints)
-
-# generate_neighbours()
 
 draw_keypoints()
 
